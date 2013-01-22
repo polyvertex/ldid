159268ec1c39478938a44fb9e5a876922cdd68e7
polyvertex <polyvertex@gmail.com>
2013-01-22 17:31:13 +0100

    Unintrusive modifications to make it work under Cygwin.

--- make.sh
+++ make.sh
@@ -9,11 +9,13 @@ if [[ -e $sdk ]]; then
     flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
 fi
 
-for arch in i386 x86_64 ppc armv6; do
-    if g++ -arch "${arch}" --version &>/dev/null; then
-        flags+=(-arch "${arch}")
-    fi
-done
+if [ -z "$(uname -s | grep -i cygwin)" ]; then
+    for arch in i386 x86_64 ppc armv6; do
+        if g++ -arch "${arch}" --version &>/dev/null; then
+            flags+=(-arch "${arch}")
+        fi
+    done
+fi
 
 set -x
 g++ "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
